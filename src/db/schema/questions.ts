0b7a54820d9e3b16f17893d5e949c1e163fae997
marciosc83 <marciosc@gmail.com>
2025-07-09 21:50:48 -0300

    feat: add create question, create room and get room questions

---
+++ src/db/schema/questions.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, uuid, timestamp } from 'drizzle-orm/pg-core';
+import { rooms } from './rooms.ts';
+
+export const questions = pgTable('questions', {
+  id: uuid().primaryKey().defaultRandom(),
+  roomId: uuid().references(() => rooms.id),
+  question: text().notNull(),
+  answer: text(),
+  createdAt: timestamp().defaultNow().notNull(),
+});
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider
+}
+from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors' 
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+    return 'OK'
+})
+
+app.listen({ port: env.PORT })
+
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
